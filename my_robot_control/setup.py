c65385986b0b5c6e3c91156a2f5e242467840290
BaMax_Badhon <85755347+hm-badhon@users.noreply.github.com>
2024-03-27 16:28:56 +0600

    Add files via upload

---
+++ my_robot_control/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_robot_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nsl47',
+    maintainer_email='h.m.badhoneee@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "test_node = my_robot_control.my_first_py_file:main"
+        ],
+    },
+)
